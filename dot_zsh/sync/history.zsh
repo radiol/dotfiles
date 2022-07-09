1fbbfa95231b9aa1719b598ff19b0268857cee2e
radiol <xillionec.zillionfw+GH@gmail.com>
2022-07-09 07:54:32 +0000

    ignore history (rm, cd, exit, etc...) add gitignore

--- dot_zsh/sync/history.zsh
+++ dot_zsh/sync/history.zsh
@@ -3,3 +3,7 @@ export HISTSIZE=10000
 export SAVEHIST=100000
 setopt hist_ignore_dups
 setopt EXTENDED_HISTORY
+zshaddhistory() {
+    local line="${1%%$'\n'}"
+    [[ ! "$line" =~ "^(cd|jj?|lazygit|la|ll|ls|rm|rmdir|exit)($| )" ]]
+}
