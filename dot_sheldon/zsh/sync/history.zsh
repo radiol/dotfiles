933e3eee4ee165b99a93824923a4753ae0e9cbc5
radiol <xillionec.zillionfw+GH@gmail.com>
2022-07-11 01:54:45 +0000

    Change theme powerlevel10k

--- dot_sheldon/zsh/sync/history.zsh
+++ dot_sheldon/zsh/sync/history.zsh
@@ -1,9 +1,12 @@
 export HISTFILE=${HOME}/.zsh_history
 export HISTSIZE=10000
-export SAVEHIST=100000
+export SAVEHIST=10000
 setopt hist_ignore_dups
 setopt EXTENDED_HISTORY
 zshaddhistory() {
     local line="${1%%$'\n'}"
     [[ ! "$line" =~ "^(cd|jj?|lazygit|la|ll|ls|rm|rmdir|exit)($| )" ]]
 }
+# enable prefix match
+bindkey ${key[Up]} history-beginning-search-backward
+bindkey ${key[Down]} history-beginning-search-forward
