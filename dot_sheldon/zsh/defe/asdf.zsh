710879f039b2dfbd82871996c20fc12b2ca6d850
radiol <xillionec.zillionfw+GH@gmail.com>
2022-07-11 10:43:45 +0000

    asdf用のロードコマンドを追加(brewからのインストール前提)

---
+++ dot_sheldon/zsh/defe/asdf.zsh
@@ -0,0 +1,17 @@
+# If not found, check for Homebrew package
+if type brew &>/dev/null; then
+  brew_prefix="$(brew --prefix asdf)"
+  ASDF_DIR="${brew_prefix}/libexec"
+  ASDF_COMPLETIONS="${brew_prefix}/etc/bash_completion.d"
+  unset brew_prefix
+fi
+
+# Load command
+if [[ -f "$ASDF_DIR/asdf.sh" ]]; then
+  . "$ASDF_DIR/asdf.sh"
+
+  # Load completions
+  if [[ -f "$ASDF_COMPLETIONS/asdf.bash" ]]; then
+    . "$ASDF_COMPLETIONS/asdf.bash"
+  fi
+fi
