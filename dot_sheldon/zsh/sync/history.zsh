8b64b5582ba0ae6f715ded5f1c1bb0066cfdd9b6
yuji <radiol@github.com>
2022-07-13 02:19:06 +0900

    余分な行を削除

--- dot_sheldon/zsh/sync/history.zsh
+++ dot_sheldon/zsh/sync/history.zsh
@@ -10,5 +10,3 @@ zshaddhistory() {
 # enable prefix match
 bindkey "^[[A" history-beginning-search-backward
 bindkey "^[[B" history-beginning-search-forward
-#bindkey "^P" history-beginning-search-backward
-#bindkey "^N" history-beginning-search-forward
