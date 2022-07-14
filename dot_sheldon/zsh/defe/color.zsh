f67d8baabff70f5ccc54ddb8715dabcbc2d7e52c
yuji <radiol@github.com>
2022-07-14 10:28:03 +0900

    update ls color

--- dot_sheldon/zsh/defe/color.zsh
+++ dot_sheldon/zsh/defe/color.zsh
@@ -1,2 +1,7 @@
 # Change dircolor blue -> cyan
 export LSCOLORS=gxfxcxdxbxegedabagacad
+
+# Enable LSCOLORS to completion
+if [ -n "$LSCOLORS" ]; then
+    zstyle ':completion:*' list-colors ${(s.:.)LSCOLORS}
+fi
