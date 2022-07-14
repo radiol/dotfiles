edd6653aa205c0604b1a3e07b0037d98ee56c0f0
yuji <radiol@github.com>
2022-07-14 15:16:26 +0900

    Move .sheldon/zsh -> .config/zsh

--- dot_sheldon/zsh/defe/color.zsh
+++
@@ -1,7 +0,0 @@
-# Change dircolor blue -> cyan
-#export LSCOLORS=gxfxcxdxbxegedabagacad
-
-## Enable LSCOLORS to completion
-#if [ -n "$LSCOLORS" ]; then
-#    zstyle ':completion:*' list-colors ${(s.:.)LSCOLORS}
-#fi
