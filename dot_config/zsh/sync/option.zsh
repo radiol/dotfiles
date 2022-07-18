27c13545a05f273850170b59873e55ca3e3b7e50
yuji <radiol@github.com>
2022-07-18 18:59:20 +0900

    Move XDG config -> .zshrc

--- dot_config/zsh/sync/option.zsh
+++ dot_config/zsh/sync/option.zsh
@@ -10,32 +10,6 @@ export VIEWER="nvim"
 export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=180'
 
 # ---------------------------------------------------------
-# XDG
-# ---------------------------------------------------------
-# basedir defaults, in case they're not already set up.
-if [[ -z "$XDG_DATA_HOME" ]]; then
-	export XDG_DATA_HOME="$HOME/.local/share"
-fi
-
-if [[ -z "$XDG_CONFIG_HOME" ]]; then
-	export XDG_CONFIG_HOME="$HOME/.config"
-fi
-
-if [[ -z "$XDG_CACHE_HOME" ]]; then
-	export XDG_CACHE_HOME="$HOME/.cache"
-fi
-
-if [[ -z "$XDG_DATA_DIRS" ]]; then
-	export XDG_DATA_DIRS="/usr/local/share:/usr/share"
-fi
-
-if [[ -z "$XDG_CONFIG_DIRS" ]]; then
-	export XDG_CONFIG_DIRS="/etc/xdg"
-else
-	export XDG_CONFIG_DIRS="/etc/xdg:$XDG_CONFIG_DIRS"
-fi
-
-# ---------------------------------------------------------
 # Path
 # ---------------------------------------------------------
 export PATH="/Users/yuji/.local/bin:$PATH"
