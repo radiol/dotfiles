499d03597ef4678ef837d91717e1843de898366c
yuji <radiol@github.com>
2022-07-31 16:38:36 +0900

    Stable(Mason, nvim-cmp, lsp_lines)

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -209,7 +209,11 @@ local mason_lspconfig = require("mason-lspconfig")
 mason_lspconfig.setup()
 mason_lspconfig.setup_handlers({
   function(server_name)
-    local opts = {}
+    local opts = {
+      capabilities = require('cmp_nvim_lsp').update_capabilities(
+        vim.lsp.protocol.make_client_capabilities()
+      )
+    }
 
     if server_name == "sumneko_lua" then
       opts.settings = {
