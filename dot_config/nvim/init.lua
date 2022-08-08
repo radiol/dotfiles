c0f5a7de7a3bf4f441df868b205d5757b37e9cd7
yuji <radiol@github.com>
2022-08-08 16:07:45 +0900

    Add LunerVim installation

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -191,7 +191,7 @@ require("mason.settings").set({
 })
 
 local lsp = require("lsp-zero")
-lsp.preset("recommended")
+lsp.preset("lsp-only")
 -- lua language server config
 lsp.configure("sumneko_lua", {
 	settings = {
@@ -207,49 +207,49 @@ lsp.setup()
 -- -- Nvim-cmp setting
 -- -----------------------------------------------------------
 local cmp = require("cmp")
---
--- cmp.setup({
--- 	snippet = {
--- 		expand = function(args)
--- 			require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
--- 		end,
--- 	},
--- 	window = {
--- 		completion = cmp.config.window.bordered(),
--- 		documentation = cmp.config.window.bordered(),
--- 	},
--- 	mapping = cmp.mapping.preset.insert({
--- 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
--- 		["<C-f>"] = cmp.mapping.scroll_docs(4),
--- 		["<C-Space>"] = cmp.mapping.complete(),
--- 		["<C-e>"] = cmp.mapping.abort(),
--- 		["<CR>"] = cmp.mapping.confirm({
--- 			behavior = cmp.ConfirmBehavior.Replace,
--- 			select = true,
--- 		}),
--- 		["<Tab>"] = function(fallback)
--- 			if cmp.visible() then
--- 				cmp.select_next_item()
--- 			else
--- 				fallback()
--- 			end
--- 		end,
--- 		["<S-Tab>"] = function(fallback)
--- 			if cmp.visible() then
--- 				cmp.select_prev_item()
--- 			else
--- 				fallback()
--- 			end
--- 		end,
--- 	}),
--- 	sources = cmp.config.sources({
--- 		{ name = "nvim_lsp" },
--- 		{ name = "luasnip" }, -- For luasnip users.
--- 	}, {
--- 		{ name = "buffer" },
--- 	}),
--- })
---
+
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+		end,
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = true,
+		}),
+		["<Tab>"] = function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			else
+				fallback()
+			end
+		end,
+		["<S-Tab>"] = function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			else
+				fallback()
+			end
+		end,
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" }, -- For luasnip users.
+	}, {
+		{ name = "buffer" },
+	}),
+})
+
 -- Set configuration for specific filetype.
 cmp.setup.filetype("gitcommit", {
 	sources = cmp.config.sources({
