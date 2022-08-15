86ae9e68ad374dbf2fa227c651790c2e1c541bfa
yuji <radiol@github.com>
2022-08-15 23:49:05 +0900

    color.lua: Add install check.

--- dot_config/nvim/lua/color.lua
+++ dot_config/nvim/lua/color.lua
@@ -1,4 +1,9 @@
 -----------------------------------------------------------
 -- colorscheme
 -----------------------------------------------------------
+local status, theme = pcall(require, "tokyonight")
+if not status then
+	return
+end
+
 vim.cmd([[colorscheme tokyonight]])
