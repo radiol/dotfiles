0372eae6153c02383d8f191b3fa80b1857bf095d
radiol <46495806+radiol@users.noreply.github.com>
2022-10-05 11:36:20 +0900

    Create installer for manjaro.

---
+++ install_manjaro.sh
@@ -0,0 +1,9 @@
+# Change mirror server
+sudo pacman-mirrors --fasttrack
+
+# Update
+sudo pacman -Syyu  --noconfirm
+
+# Change dir names to English
+sudo pacman -Sy xdg-user-dirs-gtk --noconfirm
+LANG=C xdg-user-dirs-gtk-update
