aa9e54c881a9a6a79d83ffb729845c20dd691806
radiol <46495806+radiol@users.noreply.github.com>
2022-10-05 12:02:07 +0900

    update

--- install_manjaro.sh
+++ install_manjaro.sh
@@ -7,3 +7,15 @@ sudo pacman -Syyu  --noconfirm
 # Change dir names to English
 sudo pacman -Sy xdg-user-dirs-gtk --noconfirm
 LANG=C xdg-user-dirs-gtk-update
+
+# Change CapsLock to Ctrl
+/usr/bin/setxkbmap -option "ctrl:nocaps"
+
+# Install applications
+sudo pacman -S --noconfirm \
+  manjaro-asian-input-support-ibus \
+  gtk2 \
+  ufw \
+  yay \
+  
+yay -S ibus-mozc
