f29ed29cc313779c163040381b785792b00ad752
yuji <radiol@github.com>
2022-10-30 22:43:27 +0900

    clipboard alias "C"

--- dot_config/zsh/sync/mac_linux_compatible.zsh
+++ dot_config/zsh/sync/mac_linux_compatible.zsh
@@ -2,15 +2,23 @@
 
 # Get operating system
 platform="unknown"
+mac="darwin"
+linux="linux"
 unamestr=$(uname)
-if [[ $unamestr == "Linux" ]]; then
-    platform="linux"
-elif [[ $unamestr == "Darwin" ]]; then
-    platform="darwin"
+if [[ $unamestr == "Darwin" ]]; then
+  platform=mac
+elif [[ $unamestr == "Linux" ]]; then
+  platform=linux
 fi
 
 # dircolors(Ubuntu) <-> gdircolors(macOS)
-if [[ $platform == "darwin" ]]; then
-    alias dircolors="gdircolors"
+if [[ $platform == mac ]]; then
+  alias dircolors="gdircolors"
 fi
 
+# clipboard
+if [[ $platform == mac ]]; then
+  alias C="pbcopy"
+elif [[ $platform == linux ]]; then
+  alias C="xsel -b"
+fi
