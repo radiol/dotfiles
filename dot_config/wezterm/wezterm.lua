7ea8274cc276aa2d96a5b0a6470cba57d3b7072a
yuji <radiol@github.com>
2022-10-31 18:08:23 +0900

    Wezterm SSH domains config for multiplexing.

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,5 +1,18 @@
 local wezterm = require("wezterm")
 
+ssh_domains = {
+	{
+		name = "node",
+		remote_address = "node",
+	},
+}
+
+unix_domains = {
+	{
+		name = "unix",
+	},
+}
+
 return {
 	initial_cols = 100,
 	initial_rows = 30,
@@ -8,4 +21,7 @@ return {
 	color_scheme = "tokyonight",
 	window_background_opacity = 0.8,
 	send_composed_key_when_left_alt_is_pressed = true,
+	ssh_domains = ssh_domains,
+	unix_domains = unix_domains,
+	default_gui_startup_args = { "connect", "unix" },
 }
