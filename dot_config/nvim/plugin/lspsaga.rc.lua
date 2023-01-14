38b83377bf8f5960976d97bdbc761a3678f58d21
yuji <radiol@github.com>
2023-01-15 01:30:58 +0900

    Lspsaga breaking changes.

--- dot_config/nvim/plugin/lspsaga.rc.lua
+++ dot_config/nvim/plugin/lspsaga.rc.lua
@@ -6,14 +6,14 @@ if not status then
 	return
 end
 
-saga.init_lsp_saga({})
+saga.setup({})
 
 local opts = { noremap = true, silent = true }
 -- lsp
 vim.keymap.set("n", "K", "<Cmd>Lspsaga hover_doc<CR>", opts)
 vim.keymap.set("n", "gd", "<Cmd>Lspsaga lsp_finder<CR>", opts)
 vim.keymap.set("i", "gs", "<Cmd>Lspsaga signature_help<CR>", opts)
-vim.keymap.set("n", "gp", "<Cmd>Lspsaga preview_definition<CR>", opts)
+vim.keymap.set("n", "gp", "<Cmd>Lspsaga goto_definition<CR>", opts)
 vim.keymap.set("n", "gr", "<Cmd>Lspsaga rename<CR>", opts)
 -- diagnostic
 vim.keymap.set("n", "gl", "<Cmd>Lspsaga show_line_diagnostics<CR>")
