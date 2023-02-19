87705e655062a42471be809b529e6fa52d51dc7c
yuji <radiol@github.com>
2023-02-20 01:17:26 +0900

    Update run_once_install.sh

--- run_once_install.sh
+++ run_once_install.sh
@@ -52,6 +52,16 @@ if [ $platform == "ubuntu" ] || [ $platform == "darwin" ]; then
     fi
 fi
 
+# Activate Homebrew path
+# if [[ -d "/home/linuxbrew/.linuxbrew" ]]; then
+#     HOMEBREW_HOME="/home/linuxbrew/.linuxbrew"
+# elif [[ -d "/opt/homebrew" ]]; then
+#     HOMEBREW_HOME="/opt/homebrew"
+# else
+#     HOMEBREW_HOME="/usr/local"
+# fi
+eval "($HOMEBREW_HOME/bin/brew shellenv)"
+
 # ---------------------------------------------------------
 # Install asdf, cargo, volta
 # ---------------------------------------------------------
