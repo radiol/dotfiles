bd7871c04acd9c953412055d5155f81a65a0640f
yuji <radiol@github.com>
2023-02-19 18:43:12 +0900

    Create run_once_install.sh

---
+++ run_once_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -xeu
+
+# Get operating system
+platform="unknown"
+unamestr=$(uname)
+if [[ $unamestr == "Linux" ]]; then
+    if [[ -e /etc/debian_version ]]; then
+        platform="ubuntu"
+    elif [[ -e /etc/manjaro-release ]]; then
+        platform="manjaro"
+    elif [[ -e /etc/arch-release ]]; then
+        platform="arch"
+    fi
+elif [[ $unamestr == "Darwin" ]]; then
+    platform="darwin"
+fi
+echo $platform
