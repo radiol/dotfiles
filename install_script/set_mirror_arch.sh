1e79c46f641f4b84043b1b2848a6c5f689572863
yuji <radiol@github.com>
2023-02-20 22:46:34 +0900

    Delete old install scripts

--- install_script/set_mirror_arch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -xeu
-mkdir -p ~/.cache
-sudo pacman -Sy --noconfirm reflector
-sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-sudo reflector --country "Japan,Australia" --age 24 --latest 5 --protocol https --sort score --save /etc/pacman.d/mirrorlist
-
--- install_script/set_mirror_manjaro.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -xeu
-mkdir -p ~/.cache
-# Change mirror server
-sudo pacman-mirrors --fasttrack
