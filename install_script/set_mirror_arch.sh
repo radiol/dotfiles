6b479b70c07933b64a88f9b78d21e8a2d4dbcfbe
yuji <radiol@github.com>
2023-02-20 16:02:31 +0900

    reflectorのオプションを変更

--- install_script/set_mirror_arch.sh
+++ install_script/set_mirror_arch.sh
@@ -2,5 +2,5 @@
 mkdir -p ~/.cache
 sudo pacman -Sy --noconfirm reflector
 sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-sudo reflector --country "Japan" --age 24 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+sudo reflector --country "Japan,Australia" --age 24 --latest 5 --protocol https --sort score --save /etc/pacman.d/mirrorlist
 
