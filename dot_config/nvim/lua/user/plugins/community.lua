57b5af2403bf313b9eaecc1752c14f1956e87f31
radiol <radiol@github.com>
2023-06-27 21:30:30 +0900

    Pythonの仮想環境検索に".venv/"を追加。<leader>lv

--- dot_config/nvim/lua/user/plugins/community.lua
+++ dot_config/nvim/lua/user/plugins/community.lua
@@ -5,6 +5,10 @@ return {
   --Language Pack
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.python" },
+  {
+    "linux-cultist/venv-selector.nvim",
+    opts = { name = { "venv", ".venv" } },
+  },
   { import = "astrocommunity.pack.rust" },
   --Editing Support
   { import = "astrocommunity.editing-support.suda-vim" },
