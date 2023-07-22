0fd335ef79a04ad51d50040d75d1c4defc923680
radiol <radiol@github.com>
2023-07-22 14:19:32 +0900

    AstroNvim: Add dial.nvim.

--- dot_config/nvim/lua/user/plugins/community.lua
+++ dot_config/nvim/lua/user/plugins/community.lua
@@ -1,8 +1,9 @@
 return {
   "AstroNvim/astrocommunity",
-  --colorscheme
+  -- colorscheme
   { import = "astrocommunity.colorscheme.tokyonight-nvim" },
-  --Language Pack
+
+  -- Language Pack
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.python" },
   {
@@ -10,8 +11,13 @@ return {
     opts = { name = { "venv", ".venv" } },
   },
   { import = "astrocommunity.pack.rust" },
-  --Editing Support
+
+  -- Editing Support
+  -- <Leader> + Shift + w: Sudo write
   { import = "astrocommunity.editing-support.suda-vim" },
-  --Utility
+  -- Ctrl + a or x: Increment/decrement
+  { import = "astrocommunity.editing-support.dial-nvim" },
+
+  -- Utility
   { import = "astrocommunity.utility.noice-nvim" },
 }
