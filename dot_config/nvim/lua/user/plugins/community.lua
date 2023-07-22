e7fdf997564b556f7f640adbee37d3ab06b210e2
radiol <radiol@github.com>
2023-07-23 00:04:07 +0900

    AstroNvim: Add flash.nvim

--- dot_config/nvim/lua/user/plugins/community.lua
+++ dot_config/nvim/lua/user/plugins/community.lua
@@ -13,10 +13,15 @@ return {
   { import = "astrocommunity.pack.rust" },
 
   -- Editing Support
-  -- <Leader> + Shift + w: Sudo write
+  -- <leader> + Shift + w: Sudo write
   { import = "astrocommunity.editing-support.suda-vim" },
-  -- Ctrl + a or x: Increment/decrement
+  -- Ctrl + a or x: Increment/Decrement
   { import = "astrocommunity.editing-support.dial-nvim" },
+
+  -- Motion
+  -- "/"検索での検索結果にアルファベットのインデックスが追加
+  -- 検索途中でそのアルファベットのキーを押すことでそこに飛ぶ
+  { import = "astrocommunity.motion.flash-nvim" },
 
   -- Utility
   { import = "astrocommunity.utility.noice-nvim" },
