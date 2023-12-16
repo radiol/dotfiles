135052dab7a58f10af6f2a971dd622f431580953
radiol <radiol@github.com>
2023-12-17 02:39:00 +0900

    LunarVim: mninor change

--- dot_config/lvim/after/ftplugin/python.lua
+++ dot_config/lvim/after/ftplugin/python.lua
@@ -1,7 +1,7 @@
-local manager = require "lvim.lsp.manager"
-local formatter = require "lvim.lsp.null-ls.formatters"
-local linter = require "lvim.lsp.null-ls.linters"
+local manager = require("lvim.lsp.manager")
+local formatter = require("lvim.lsp.null-ls.formatters")
+local linter = require("lvim.lsp.null-ls.linters")
 
 manager.setup("ruff_lsp")
-formatter.setup { { name = "black" } }
-linter.setup { { command = "ruff", filetypes = { "python" } } }
+formatter.setup({ { name = "black" } })
+linter.setup({ { command = "ruff", filetypes = { "python" } } })
