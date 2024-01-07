2c86e6651e6327742ea127782cee9d52832d38c5
radiol <radiol@github.com>
2024-01-07 19:44:12 +0900

    Update terminal directory to current working directory

--- dot_config/lvim/lua/user/options.lua
+++ dot_config/lvim/lua/user/options.lua
@@ -5,4 +5,5 @@ lvim.colorscheme = "tokyonight"
 
 lvim.format_on_save.enabled = true
 lvim.builtin.terminal.open_mapping = "<c-t>"
+lvim.builtin.terminal.dir = vim.fn.getcwd()
 lvim.builtin.nvimtree.active = false -- NOTE: using neo-tree
