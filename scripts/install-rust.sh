f472e221a6b8c3203841e33e7663fde290eddf27
radiol <radiol@github.com>
2024-08-11 20:22:20 +0900

    Split to install-rust.sh

---
+++ scripts/install-rust.sh
@@ -0,0 +1,25 @@
+# This script installs Rust and some Rust apps
+if ! (type "cargo" > /dev/null 2>&1); then
+    curl --proto '=https' --tlsv1.2 https://sh.rustup.rs > rustup.sh
+    sh rustup.sh -y -q --no-modify-path && rm rustup.sh
+    source "$HOME/.cargo/env"
+fi
+
+# Install Rust Apps
+cargo install -q cargo-binstall
+cargo binstall -q  -y --only-signed\
+    bp \
+    cargo-edit \
+    cargo-update \
+    eza \
+    git-delta \
+    sheldon \
+    starship \
+    topgrade \
+    xcp \
+    zoxide
+
+# Install Trashy (Linux only)
+if [ "$(uname)" == "Linux" ]; then
+    cargo binstall -q -y --only-signed trashy
+fi
