0f461b4596eb04f46ba75e9d43c5a17415012ae2
radiol <radiol@github.com>
2024-09-07 01:29:23 +0900

    Fix brew install option

--- scripts/install-uv.sh
+++ scripts/install-uv.sh
@@ -14,7 +14,7 @@ echo "Installing uv..."
 if type pacman >/dev/null 2>&1; then
   sudo pacman -S --noconfirm uv
 elif type brew >/dev/null 2>&1; then
-  brew install -y uv
+  brew install -q uv
 else
   INSTALLER_NO_MODIFY_PATH=1 bash -c 'curl -LsSf https://astral.sh/uv/install.sh | sh'
 fi
