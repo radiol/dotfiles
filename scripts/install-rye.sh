a30f5c2188630ff613455fa22cf3b57c85f16342
radiol <radiol@github.com>
2024-09-07 01:12:34 +0900

    Update installation script to install uv instead of rye

--- scripts/install-rye.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -xeu
-
-# ---------------------------------------------------------
-# Install Rye
-# ---------------------------------------------------------
-echo "Installing Rye"
-curl -sSf https://rye.astral.sh/get | RYE_INSTALL_OPTION="--yes" bash
