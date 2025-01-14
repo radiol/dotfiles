6d24bf9a9f2224371245aca3aae074c0968e965f
radiol <radiol@github.com>
2025-01-14 13:55:15 +0900

    Suppress code actions from null-ls in noice plugin

--- dot_config/nvim/lua/plugins/noice.lua
+++ dot_config/nvim/lua/plugins/noice.lua
@@ -8,6 +8,17 @@ return {
       presets = {
         bottom_search = false,
       },
+      -- Don't show code actions from null-ls
+      -- 改行のたびにnull-lsがcode actionを出してくるので、それを抑制する
+      routes = {
+        {
+          filter = {
+            event = "lsp",
+            find = "code_action null%-ls",
+          },
+          opts = { skip = true },
+        },
+      },
     },
   },
 }
