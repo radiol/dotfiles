b3acb5f7173b6544c37195ddb1ea49b765843ff9
radiol <radiol@github.com>
2025-01-31 01:08:18 +0900

    Add script to install go apps

---
+++ scripts/go.sh
@@ -0,0 +1,9 @@
+# Check go is available
+if ! type go >/dev/null 2>&1; then
+  echo "go is not available"
+  exit 0
+fi
+
+# Install go apps
+echo "Installing go apps"
+go install github.com/babarot/gomi@latest
