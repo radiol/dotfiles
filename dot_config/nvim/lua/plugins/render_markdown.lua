ba3ecf1f282e1ae517e08f1d48a261daf938185f
radiol <radiol@github.com>
2025-02-16 00:08:12 +0900

    feat: Add render_modes and code options for markdown rendering

--- dot_config/nvim/lua/plugins/render_markdown.lua
+++ dot_config/nvim/lua/plugins/render_markdown.lua
@@ -6,6 +6,12 @@ return {
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
-    opts = {},
+    ft = { "markdown" },
+    opts = {
+      render_modes = true,
+      code = {
+        width = "block",
+      },
+    },
   },
 }
