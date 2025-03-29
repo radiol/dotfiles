b0f17dd03a0f391a5d34b95c9aecdbb915b2cc76
radiol <radiol@github.com>
2025-03-30 03:38:17 +0900

    feat: Add completion options for blink plugin. auto_insert false, ghost_text true

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -4,17 +4,18 @@ return {
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
+    completion = {
+      ghost_text = { enabled = true },
+      list = { selection = { preselect = false, auto_insert = false } },
+    },
     cmdline = {
       enabled = true,
       completion = {
         menu = {
           auto_show = true,
         },
-        list = { selection = { preselect = false } },
+        list = { selection = { preselect = false, auto_insert = true } },
       },
-    },
-    completion = {
-      list = { selection = { preselect = false } },
     },
   },
 }
