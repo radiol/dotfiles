6d1dd4bf9bbe5e9845bc56c42bc52f3116a95653
radiol <radiol@github.com>
2025-03-30 22:34:21 +0900

    feat: Update snacks.lua with scroll animation options

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -1,21 +1,24 @@
 return {
-	"snacks.nvim",
-	---@type snacks.Config
-	opts = {
-		indent = {
-			enabled = true,
-			chunk = {
-				enabled = true,
-				char = {
-					-- corner_top = "┌",
-					-- corner_bottom = "└",
-					corner_top = "╭",
-					corner_bottom = "╰",
-					horizontal = "─",
-					vertical = "│",
-					arrow = ">",
-				},
-			},
-		},
-	},
+  "snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    indent = {
+      enabled = true,
+      chunk = {
+        enabled = true,
+        char = {
+          -- corner_top = "┌",
+          -- corner_bottom = "└",
+          corner_top = "╭",
+          corner_bottom = "╰",
+          horizontal = "─",
+          vertical = "│",
+          arrow = ">",
+        },
+      },
+    },
+    scroll = {
+      enabled = true,
+    },
+  },
 }
