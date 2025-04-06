f41192a7c956d15cae6db008e5c16444e467e8f6
radiol <radiol@github.com>
2025-04-07 00:48:00 +0900

    feat: Update customization of dashboard options

--- dot_config/nvim/lua/plugins/user.lua
+++ dot_config/nvim/lua/plugins/user.lua
@@ -17,28 +17,42 @@ return {
   -- == Examples of Overriding Plugins ==
 
   -- customize dashboard options
-  -- {
-  --   "folke/snacks.nvim",
-  --   opts = {
-  --     dashboard = {
-  --       preset = {
-  --         header = table.concat({
-  --           " █████  ███████ ████████ ██████   ██████ ",
-  --           "██   ██ ██         ██    ██   ██ ██    ██",
-  --           "███████ ███████    ██    ██████  ██    ██",
-  --           "██   ██      ██    ██    ██   ██ ██    ██",
-  --           "██   ██ ███████    ██    ██   ██  ██████ ",
-  --           "",
-  --           "███    ██ ██    ██ ██ ███    ███",
-  --           "████   ██ ██    ██ ██ ████  ████",
-  --           "██ ██  ██ ██    ██ ██ ██ ████ ██",
-  --           "██  ██ ██  ██  ██  ██ ██  ██  ██",
-  --           "██   ████   ████   ██ ██      ██",
-  --         }, "\n"),
-  --       },
-  --     },
-  --   },
-  -- },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          pick = function(cmd, opts) return LazyVim.pick(cmd, opts)() end,
+          header = table.concat({
+            " █████  ███████ ████████ ██████   ██████ ",
+            "██   ██ ██         ██    ██   ██ ██    ██",
+            "███████ ███████    ██    ██████  ██    ██",
+            "██   ██      ██    ██    ██   ██ ██    ██",
+            "██   ██ ███████    ██    ██   ██  ██████ ",
+            "",
+            "███    ██ ██    ██ ██ ███    ███",
+            "████   ██ ██    ██ ██ ████  ████",
+            "██ ██  ██ ██    ██ ██ ██ ████ ██",
+            "██  ██ ██  ██  ██  ██ ██  ██  ██",
+            "██   ████   ████   ██ ██      ██",
+          }, "\n"),
+       -- stylua: ignore
+       ---@type snacks.dashboard.Item[]
+       keys = {
+         { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+         { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+         { icon = "", key = "p", desc = "Projects", action = ":lua Snacks.dashboard.pick('projects')" },
+         { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+         { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+         -- { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+         { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+         { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+         { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+       },
+        },
+      },
+    },
+  },
 
   -- You can disable default plugins as follows:
   -- { "max397574/better-escape.nvim", enabled = false },
