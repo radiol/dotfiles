167fa3f0e34dff5f756940f5d0cd9106e8a3bd60
radiol <radiol@github.com>
2025-07-22 10:21:56 +0900

    refactor: Merge blink-copilot config

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -1,9 +1,37 @@
 return {
   "blink.cmp",
+  dependencies = {
+    "fang2hou/blink-copilot",
+    "mikavilpas/blink-ripgrep.nvim",
+    "folke/lazydev.nvim",
+  },
   event = "InsertEnter",
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
+    sources = {
+      default = { "copilot", "snippets", "lsp", "ripgrep", "path", "buffer" },
+      per_filetype = {
+        lua = { inherit_defaults = true, "lazydev" },
+      },
+      providers = {
+        copilot = {
+          name = "copilot",
+          module = "blink-copilot",
+          score_offset = 100,
+          async = true,
+        },
+        ripgrep = {
+          module = "blink-ripgrep",
+          name = "Ripgrep",
+        },
+        lazydev = {
+          name = "LazyDev",
+          module = "lazydev.integrations.blink",
+          score_offset = 200,
+        },
+      },
+    },
     completion = {
       ghost_text = { enabled = true },
       list = { selection = { preselect = false, auto_insert = false } },
@@ -18,4 +46,5 @@ return {
       },
     },
   },
+  opts_extend = { "sources.default" },
 }
