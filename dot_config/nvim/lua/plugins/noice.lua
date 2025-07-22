a942324625e2941a9a7dace365d6a23b746a4d50
radiol <radiol@github.com>
2025-07-22 10:22:28 +0900

    refactor: Update routes in noice plugin configuration

--- dot_config/nvim/lua/plugins/noice.lua
+++ dot_config/nvim/lua/plugins/noice.lua
@@ -1,9 +1,15 @@
 return {
   "folke/noice.nvim",
-  event = "VeryLazy",
   opts = {
-    presets = {
-      -- bottom_search = false,
+    routes = {
+      {
+        filter = {
+          event = "lsp",
+          kind = "progress",
+          find = "code_action", -- メッセージに "code_action" を含むものを対象
+        },
+        opts = { skip = true }, -- 表示しない
+      },
     },
   },
 }
