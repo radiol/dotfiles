e63bccfd308626bf72053cfc324f0ebc08a1f497
radiol <radiol@github.com>
2025-07-27 18:42:18 +0900

    fix: Update Mise activation to use bash instead of zsh

--- scripts/mise.sh
+++ scripts/mise.sh
@@ -8,7 +8,7 @@ echo "::group::Install Mise"
 if ! (type "mise" >/dev/null 2>&1); then
 	echo "Installing Mise"
 	curl https://mise.run | sh
-	eval "$(~/.local/bin/mise activate zsh)"
+	eval "$(~/.local/bin/mise activate bash)"
 fi
 
 echo "::endgroup::"
