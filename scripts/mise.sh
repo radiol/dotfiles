efdd521cd29702ada158f2b4e126885762417f6f
radiol <radiol@github.com>
2025-07-27 18:29:38 +0900

    feat: Activate Mise for zsh in installation script

--- scripts/mise.sh
+++ scripts/mise.sh
@@ -8,6 +8,7 @@ echo "::group::Install Mise"
 if ! (type "mise" >/dev/null 2>&1); then
 	echo "Installing Mise"
 	curl https://mise.run | sh
+	eval "$(~/.local/bin/mise activate zsh)"
 fi
 
 echo "::endgroup::"
