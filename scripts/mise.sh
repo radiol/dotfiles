75dfd951a1ecd941d3b39501ec3f3fa7c4bf89ac
radiol <radiol@github.com>
2025-07-29 01:33:25 +0900

    refactor: Use guard

--- scripts/mise.sh
+++ scripts/mise.sh
@@ -5,9 +5,13 @@
 # ---------------------------------------------------------
 echo "::group::Install Mise"
 
-if ! (type "mise" >/dev/null 2>&1); then
-	echo "Installing Mise"
-	curl https://mise.run | sh
+# Check if Mise is already installed
+if type "mise" >/dev/null 2>&1; then
+	echo "Mise is already installed"
+	return
 fi
 
+# Install Mise
+echo "Installing Mise"
+curl https://mise.run | sh
 echo "::endgroup::"
