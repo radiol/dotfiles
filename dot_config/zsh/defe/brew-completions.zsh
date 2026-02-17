99af0cd32a46e1ab7acd3c84c474993d1b0a9738
radiol <radiol@github.com>
2026-02-17 15:08:31 +0900

    perf(zsh): use HOMEBREW_HOME instead of brew --prefix for faster shell startup

--- dot_config/zsh/defe/brew-completions.zsh
+++ dot_config/zsh/defe/brew-completions.zsh
@@ -1,6 +1,6 @@
 if type brew &>/dev/null
 then
-  FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
+  FPATH="$HOMEBREW_HOME/share/zsh/site-functions:${FPATH}"
 
   autoload -Uz compinit
   compinit
