a657d40a76b145df31b955f3b35df18e37c6d5eb
radiol <radiol@github.com>
2026-03-01 20:36:22 +0900

    style(nvim): simplify clipboard OSC 52 function calls

--- dot_config/nvim/lua/polish.lua
+++ dot_config/nvim/lua/polish.lua
@@ -3,22 +3,21 @@
 -- fit in the normal config locations above can go here
 
 -- copy/paste system clipboard to OS clipboard
--- X/Wayland環境ではシステムクリップボード、なければOSC 52
+-- X/Wayland環境またはmacOSではシステムクリップボード、なければOSC 52
 -- OSC 52はターミナルエミュレータが対応している必要がある
-if vim.env.DISPLAY or vim.env.WAYLAND_DISPLAY then
+if vim.env.DISPLAY or vim.env.WAYLAND_DISPLAY or vim.fn.has "mac" == 1 then
   vim.opt.clipboard = "unnamedplus"
 else
   vim.g.clipboard = {
     name = "OSC 52",
     copy = {
-      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+      ["+"] = require("vim.ui.clipboard.osc52").copy "+",
+      ["*"] = require("vim.ui.clipboard.osc52").copy "*",
     },
     paste = {
-      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+      ["+"] = require("vim.ui.clipboard.osc52").paste "+",
+      ["*"] = require("vim.ui.clipboard.osc52").paste "*",
     },
   }
   vim.opt.clipboard = "unnamedplus"
 end
-
