a644269b3f69aa911702a4f59774794d5d034f3a
radiol <radiol@github.com>
2026-03-23 22:16:31 +0900

    chore(rust): add cargo development tools and fix indentation
    Add cargo-udeps, cargo-watch, cargo-nextest, cargo-audit, and
    cargo-release to the list of installed Rust tools. Also fix
    indentation from tabs to spaces.

--- scripts/rust.sh
+++ scripts/rust.sh
@@ -6,10 +6,10 @@
 echo "::group::Install Rust"
 
 if ! (type "cargo" >/dev/null 2>&1); then
-	echo "Installing Rust"
-	curl --proto '=https' --tlsv1.2 https://sh.rustup.rs >rustup.sh
-	sh rustup.sh -y -q --no-modify-path && rm rustup.sh
-	source "$HOME/.cargo/env"
+  echo "Installing Rust"
+  curl --proto '=https' --tlsv1.2 https://sh.rustup.rs >rustup.sh
+  sh rustup.sh -y -q --no-modify-path && rm rustup.sh
+  source "$HOME/.cargo/env"
 fi
 
 # Install Rust Apps
@@ -17,15 +17,20 @@ echo "Installing cargo-binstall"
 cargo install -q cargo-binstall
 echo "Installing Rust Apps"
 cargo-binstall -q -y --only-signed --locked \
-	bp \
-	cargo-edit \
-	cargo-update \
-	eza \
-	git-delta \
-	starship \
-	topgrade \
-	xcp \
-	zoxide
+  bp \
+  cargo-edit \
+  cargo-update \
+  cargo-udeps \
+  cargo-watch \
+  cargo-nextest \
+  cargo-audit \
+  cargo-release \
+  eza \
+  git-delta \
+  starship \
+  topgrade \
+  xcp \
+  zoxide
 
 cargo install -q --locked sheldon
 
