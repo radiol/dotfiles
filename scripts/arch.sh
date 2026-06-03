e730682641b726aad352ab5e8fee821b82447719
radiol <radiol@github.com>
2026-06-03 18:33:26 +0900

    feat(ci): add error trap and group to arch.sh

--- scripts/arch.sh
+++ scripts/arch.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -xeu
 
+trap 'echo "::error::file=${BASH_SOURCE[0]},line=${LINENO}::Command failed: ${BASH_COMMAND}"' ERR
+
 # ---------------------------------------------------------
 # Set Mirror Server for ArchLinux
 # ---------------------------------------------------------
+echo "::group::ArchLinux Mirror Setup"
 echo "Setting ArchLinux mirror servers"
 mkdir -p ~/.cache
 sudo pacman -Sy -q --noconfirm reflector
 sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 sudo reflector --country "Japan" --latest 5 --completion-percent 95 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+echo "::endgroup::"
